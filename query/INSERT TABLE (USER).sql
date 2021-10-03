c09c2773f24963d449c3e327377d8715d0787964
woony <clover7kso@naver.com>
2021-10-03 23:45:41 +0900

    Update Query

--- query/INSERT TABLE (USER).sql
+++ query/INSERT TABLE (USER).sql
@@ -1,3 +1,3 @@
 USE vaccine_erp;
 INSERT INTO user(id, email, password, name, phone)
-VALUES ("971220-1409514", "clover7kso@naver.com", "rkdtjddns!23", "강성운", "010-7303-7368");
+VALUES ("971220-1111111", "clover7kso@naver.com", "rkdtjddns!23", "강성운", "010-7303-7368");
