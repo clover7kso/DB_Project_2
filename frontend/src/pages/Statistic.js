26705986672ac5e2d6dd0ca66eb7b1856b7efea8
Woony <clover7kso@naver.com>
2021-11-15 03:29:59 +0900

    pushing graph

--- frontend/src/pages/Statistic.js
+++ frontend/src/pages/Statistic.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { ReactComponent as arrow_right } from '../imgs/arrow_left.svg';
+import axios from 'axios';
+import { Line } from 'react-chartjs-2';
 import {
-  VictoryChart,
-  VictoryZoomContainer,
-  VictoryLine,
-  VictoryBrushContainer,
-  VictoryAxis,
-} from 'victory';
+  CardWrapper,
+  CardBody,
+  CardHeader,
+  CardHeading,
+} from '../components/Card';
 
 const Body = styled.div`
   display: flex;
@@ -16,74 +16,69 @@ const Body = styled.div`
 const Col = styled.div``;
 
 const Statistic = ({ history }) => {
-  const [zoomDomain, setZoomDomain] = useState({
-    x: [new Date(1990, 1, 1), new Date(2009, 1, 1)],
-  });
+  const DAY = 7;
+  var [day, setDay] = useState([]);
+  useEffect(() => {
+    axios
+      .get('http://localhost:4000/byDay?day=' + DAY)
+      .then(({ data }) => setDay(data));
+  }, []);
+  console.log(day);
+
   return (
-    <Body>
-      <Col style={{ paddingLeft: '200px', width: '40%', height: '40%' }}>
-        <VictoryChart
-          width={600}
-          height={370}
-          scale={{ x: 'time' }}
-          containerComponent={
-            <VictoryZoomContainer
-              zoomDimension="x"
-              zoomDomain={zoomDomain}
-              onZoomDomainChange={setZoomDomain}
-            />
-          }
+    <Body style={{ alignItems: 'center', justifyContent: 'center' }}>
+      <Col style={{ width: '80%', height: '40%' }}>
+        <CardWrapper
+          style={{
+            width: '100%',
+          }}
         >
-          <VictoryLine
-            style={{
-              data: { stroke: 'tomato' },
-            }}
-            data={[
-              { a: new Date(1982, 1, 1), b: 125 },
-              { a: new Date(1987, 1, 1), b: 257 },
-              { a: new Date(1993, 1, 1), b: 345 },
-              { a: new Date(1997, 1, 1), b: 515 },
-              { a: new Date(2001, 1, 1), b: 132 },
-              { a: new Date(2005, 1, 1), b: 305 },
-              { a: new Date(2011, 1, 1), b: 270 },
-              { a: new Date(2015, 1, 1), b: 470 },
-            ]}
-            x="a"
-            y="b"
-          />
-        </VictoryChart>
-        <VictoryChart
-          padding={{ top: 0, left: 50, right: 50, bottom: 30 }}
-          width={600}
-          height={100}
-          scale={{ x: 'time' }}
-          containerComponent={
-            <VictoryBrushContainer
-              brushDimension="x"
-              brushDomain={zoomDomain}
-              onBrushDomainChange={setZoomDomain}
+          <CardHeader style={{ marginBottom: '0px' }}>
+            <CardHeading>최근 일주일 접종자 수</CardHeading>
+          </CardHeader>
+
+          <CardBody>
+            <Line
+              data={{
+                labels: day.map(
+                  (item) => item.y_date + '.' + item.m_date + '.' + item.d_date,
+                ),
+                datasets: [
+                  {
+                    data: day.map((item) => item.count),
+                    fill: true,
+                    borderColor: 'rgb(75,192,192)',
+                    tension: 0.1,
+                  },
+                ],
+              }}
+              options={{
+                legend: { display: false },
+                scales: {
+                  xAxes: [
+                    {
+                      gridLines: {
+                        drawOnChartArea: false,
+                      },
+                    },
+                  ],
+                  yAxes: [
+                    {
+                      gridLines: {
+                        drawOnChartArea: false,
+                      },
+                      ticks: {
+                        beginAtZero: true,
+                        stepSize: 1,
+                        min: 0,
+                      },
+                    },
+                  ],
+                },
+              }}
             />
-          }
-        >
-          <VictoryAxis tickFormat={(x) => new Date(x).getFullYear()} />
-          <VictoryLine
-            style={{
-              data: { stroke: 'tomato' },
-            }}
-            data={[
-              { key: new Date(1982, 1, 1), b: 125 },
-              { key: new Date(1987, 1, 1), b: 257 },
-              { key: new Date(1993, 1, 1), b: 345 },
-              { key: new Date(1997, 1, 1), b: 515 },
-              { key: new Date(2001, 1, 1), b: 132 },
-              { key: new Date(2005, 1, 1), b: 305 },
-              { key: new Date(2011, 1, 1), b: 270 },
-              { key: new Date(2015, 1, 1), b: 470 },
-            ]}
-            x="key"
-            y="b"
-          />
-        </VictoryChart>
+          </CardBody>
+        </CardWrapper>
       </Col>
     </Body>
   );
