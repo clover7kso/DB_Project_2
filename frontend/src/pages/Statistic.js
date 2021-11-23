ce974d8bd110bb122b481f5d7cd6b69578ce7114
Izar111 <34090234+Izar111@users.noreply.github.com>
2021-11-24 00:08:02 +0900

    Add Monthly, Location, Step Statistics
    Add Monthly : Line Chart
            Location : Bar Chart
            Step  : Pie Chart
    Statistics
    
    and Divide Component

--- frontend/src/pages/Statistic.js
+++ frontend/src/pages/Statistic.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
 import axios from 'axios';
-import { Line } from 'react-chartjs-2';
 import {
   CardWrapper,
   CardBody,
   CardHeader,
   CardHeading,
 } from '../components/Card';
+import styled from 'styled-components';
+
+import StatisticComp from '../components/StatisticComp';
 
 const Body = styled.div`
   display: flex;
@@ -16,71 +17,78 @@ const Body = styled.div`
 const Col = styled.div``;
 
 const Statistic = ({ history }) => {
+
+  //최근 일주일 접종자수
   const DAY = 7;
   var [day, setDay] = useState([]);
   useEffect(() => {
     axios
       .get('http://localhost:4000/byDay?day=' + DAY)
-      .then(({ data }) => setDay(data));
+      .then(({ data }) => setDay(data.reverse()));
   }, []);
   console.log(day);
 
-  return (
-    <Body style={{ alignItems: 'center', justifyContent: 'center' }}>
-      <Col style={{ width: '80%', height: '40%' }}>
-        <CardWrapper
-          style={{
-            width: '100%',
-          }}
-        >
-          <CardHeader style={{ marginBottom: '0px' }}>
-            <CardHeading>최근 일주일 접종자 수</CardHeading>
-          </CardHeader>
+  
+  //지역별 접종 현황
+  var [sido, setSido] = useState([]);
+  useEffect(() => {
+    axios
+      .get('http://localhost:4000/bySidoPer')
+      .then(({ data }) => {
+          var colered = [];
+          for(var i=0;i<data.length;i++){
+            colered.push({
+              sido : data[i].sido,
+              percent : data[i].접종률!=null?data[i].접종률:0,
+              //0x000000 ~ 0xFFFFFF
+              bar_coler : '#'+0xFFAAFF
+            });
+          }
+        setSido(colered);
+      });
+  }, []);
+  console.log(sido);
 
-          <CardBody>
-            <Line
-              data={{
-                labels: day.map(
-                  (item) => item.y_date + '.' + item.m_date + '.' + item.d_date,
-                ),
-                datasets: [
-                  {
-                    data: day.map((item) => item.count),
-                    fill: true,
-                    borderColor: 'rgb(75,192,192)',
-                    tension: 0.1,
-                  },
-                ],
-              }}
-              options={{
-                legend: { display: false },
-                scales: {
-                  xAxes: [
-                    {
-                      gridLines: {
-                        drawOnChartArea: false,
-                      },
-                    },
-                  ],
-                  yAxes: [
-                    {
-                      gridLines: {
-                        drawOnChartArea: false,
-                      },
-                      ticks: {
-                        beginAtZero: true,
-                        stepSize: 1,
-                        min: 0,
-                      },
-                    },
-                  ],
-                },
-              }}
-            />
-          </CardBody>
-        </CardWrapper>
-      </Col>
-    </Body>
+  //월별 접종자 수
+  const MONTH = 12;
+  var [month, setMonth] = useState([]);
+  useEffect(() => {
+    axios
+      .get('http://localhost:4000/byMonth?month=' + MONTH)
+      .then(({ data }) => setMonth(data.reverse()));
+  }, []);
+  console.log(month);
+
+  //지역별 접종 현황
+  var [step, setStep] = useState([]);
+  useEffect(() => {
+    var temp = [];
+    axios
+      .get('http://localhost:4000/byVaccination')
+      .then(({ data }) => {
+          temp.push({
+            inject : '미접종자',
+            count : data.미접종자,
+            pie_coler : '#FF5555'
+          });
+          temp.push({
+            inject : '1차 접종자',
+            count : data.접종_1차,
+            pie_coler : '#5555FF'
+          });
+          temp.push({
+            inject : '접종 완료자',
+            count : data.접종_완료자,
+            pie_coler : '#55AA55'
+          });
+          setStep(temp);
+        });
+  }, []);
+  console.log(step);
+
+  //render
+  return (
+    <StatisticComp day={day} sido={sido} month={month} step={step} ></StatisticComp>
   );
 };
 
