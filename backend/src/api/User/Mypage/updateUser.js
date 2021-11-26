a41a69cb791c0246724f113e39e96e2b9b1059ef
rang21c <rang21c@naver.com>
2021-11-27 07:32:51 +0900

    Fix updateUser

--- backend/src/api/User/Mypage/updateUser.js
+++ backend/src/api/User/Mypage/updateUser.js
@@ -3,10 +3,10 @@ import auth from "../../modules/auth.js";
 export default (app, connection) => {
   app.get('/updateUser', auth);  
   app.use('/updateUser', async (req, res, next) => {
-    const { pw, phone, sido, ssn } = req.query;
+    const { pw, phone, sido, id } = req.query;
       connection.query(
-        'UPDATE USER SET pw=?, phone=?, sido=? WHERE ssn = ?;',
-        [ pw, phone, sido, ssn ],
+        'UPDATE USER SET pw=?, phone=?, sido=? WHERE id = ?;',
+        [ pw, phone, sido, id ],
         (error, data) => {
           if (error) throw error;
           res.send({ result: true, msg: '개인정보 변경이 완료되었습니다.' });
