3b24227f1ff6a7504afc79fa449b74f127f09203
izar111 <ghktks98@gmail.com>
2021-11-27 04:29:12 +0900

    MyPageComponent Error Fix

--- frontend/src/components/MyPageComp.js
+++ frontend/src/components/MyPageComp.js
@@ -18,26 +18,32 @@ const Body = styled.div`
 const Table = styled.table`
 border:1;
 border-color:'white';
+display:'flex'
+width:'100%';
 `;
 
 const T_Col = styled.tr`
+width:'100%';
 `;
 
 const T_Row = styled.td`
 background-color:#FFFFFF;
+width:'80%';
 `;
 
 const T_RowBold = styled.th`
 background-color:#EEEEEE;
+width:'80%';
 `;
 
 const T_RowTitle = styled.th`
 background-color:#E2E2E2;
+width:'80%';
 `;
 
 class DataLine extends React.Component{
     render(){
-        var name = this.props.name;
+        var name = this.props.title;
         var value = this.props.value;
         return(
             <T_Col>
@@ -51,7 +57,9 @@ class DataLine extends React.Component{
 export class MyPageComp extends React.Component {
   render() {
     var Data = this.props.data;
+    console.log(Data);
 
+    if(Data!==undefined){
     return (
       <Body>
         <CardWrapper>
@@ -64,15 +72,15 @@ export class MyPageComp extends React.Component {
                 <T_Col>
                     <T_RowTitle colSpan="2">인적사항</T_RowTitle>
                 </T_Col>
-                <DataLine name='이름' value={Data.name}/>
-                <DataLine name='주민등록번호' value={Data.ssn}/>
-                <DataLine name='전화번호' value={Data.phone}/>
-                <DataLine name='지역' value={Data.sido}/>
+                <DataLine title='이름' value={Data.name}/>
+                <DataLine title='주민등록번호' value={Data.ssn}/>
+                <DataLine title='전화번호' value={Data.phone}/>
+                <DataLine title='지역' value={Data.sido}/>
                 <T_Col>
                     <T_RowTitle colSpan="2">아이디/패스워드</T_RowTitle>
                 </T_Col>
-                <DataLine name='아이디' value={Data.id}/>
-                <DataLine name='비밀번호' value={Data.pw}/>
+                <DataLine title='아이디' value={Data.id}/>
+                <DataLine title='비밀번호' value={Data.pw}/>
             </Table>
             <CardFieldset>
               <CardLink link="/UserUpdate">정보 수정을 원하시나요?</CardLink>
@@ -81,6 +89,7 @@ export class MyPageComp extends React.Component {
         </CardWrapper>
       </Body>
     );
+    }else return <div>로딩 중....</div>;
   }
 }
 
