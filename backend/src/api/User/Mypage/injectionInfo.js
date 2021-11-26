03d00017f9bfd5f198847558761a4c6d65f39d33
rang21c <rang21c@naver.com>
2021-11-27 06:07:11 +0900

    Fix injectionInfo.js

--- backend/src/api/User/Mypage/injectionInfo.js
+++ backend/src/api/User/Mypage/injectionInfo.js
@@ -1,14 +1,23 @@
+import auth from "../../modules/auth.js"
 export default async (app, connection) => {
-  app.get('/injectionInfo', async (req, res, next) => {
-    const { ssn } = req.query;
+  app.get('/injectionInfo', auth);  
+  app.use('/injectionInfo', async (req, res, next) => {
+    const { id } = req.query;
     await connection.query(
-      'SELECT I.inject_date, H.orgnm, V.name AS Vname, U.name AS Uname  FROM INJECTION I, USER U, HOSPITAL H, VACCINE V WHERE I.ssn = U.ssn AND I.orgcd = H.orgcd AND I.number = V.number AND U.ssn = ?;',
-      [ssn],
+      'SELECT ssn FROM USER WHERE id=?;',
+      [ id ],
       (error, data) => {
         if (error) console.log(error);
-        const result = data;
-        console.log(result);
-        return res.send(result);
+        connection.query(
+          'SELECT I.inject_date, I.reservation_time, H.orgnm, V.name AS Vname, U.name AS Uname  FROM INJECTION I, USER U, HOSPITAL H, VACCINE V WHERE I.ssn = U.ssn AND I.orgcd = H.orgcd AND I.number = V.number AND U.ssn = ?;',
+          [data.ssn],
+          (error1, data1) => {
+            if (error1) console.log(error);
+            const result1 = data1;
+            console.log(result1);
+            return res.send(result1);
+          },
+        )
       },
     );
   });
