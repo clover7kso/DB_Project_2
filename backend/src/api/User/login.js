9c4d13710018755c72ca26719486f90dc321f4df
Woony <clover7kso@naver.com>
2021-11-27 03:38:17 +0900

    fixing login bug

--- backend/src/api/User/login.js
+++ backend/src/api/User/login.js
@@ -8,10 +8,10 @@ export default (app, connection) => {
       [id, pw],
       async (error, data) => {
         if (error) throw error;
-        const result = data ? true : false;
+        const result = data.id ? true : false;
         if (result == true) {
           const jwtToken = await sign(id, pw);
-          jwtToken.name = data[0].name;
+          jwtToken.name = data.name;
           jwtToken.type = 'user';
           console.log(jwtToken);
           res.send(jwtToken);
--- backend/src/api/Vaccine/reserveVaccine.js
+++ backend/src/api/Vaccine/reserveVaccine.js
@@ -1,39 +1,37 @@
-import { sign } from '../modules/jwt.js';
-import auth from "../../modules/auth.js";
+import auth from '../../api/modules/auth.js';
 
 export default (app, connection) => {
-    app.get('/reserveVaccine', auth);
-    app.use('/reserveVaccine', (req, res, next) => {
-      const { vaccine_name, orgcd, reservation_time, id } = req.query;
-       connection.query(
-        'SELECT number, name FROM VACCINE V WHERE V.name = ? AND NOT EXISTS(SELECT number FROM INJECTION I WHERE I.number = V.number)',
-        [vaccine_name],
-        (error, data) => {
-            if (error) console.log(error);
-            const result = data;
-            console.log(result);
-            if(result.length === 0){
-                res.send(false);
-            }
-            connection.query(
-                'SELECT ssn FROM USER WHERE id=?;',
-                [id],
-                (error1, data1) => {
-                    if (error1) throw error;
-                    res.send(true);
-                },
-                connection.query(
-                    'INSERT INTO INJECTION(number, ssn, orgcd, inject_date, reservation_time) VALUES (?,?,?,NULL,?)',
-                    [result[0].number, data1.ssn, orgcd, reservation_time],
-                    (error2, data2) => {
-                        if (error2) throw error;
-                        res.send(true);
-                    },
-                )
-            );
-            console.log(result);
-        },
-      );
-    });
-  };
-  
+  app.get('/reserveVaccine', auth);
+  app.use('/reserveVaccine', (req, res, next) => {
+    const { vaccine_name, orgcd, reservation_time, id } = req.query;
+    connection.query(
+      'SELECT number, name FROM VACCINE V WHERE V.name = ? AND NOT EXISTS(SELECT number FROM INJECTION I WHERE I.number = V.number)',
+      [vaccine_name],
+      (error, data) => {
+        if (error) console.log(error);
+        const result = data;
+        console.log(result);
+        if (result.length === 0) {
+          res.send(false);
+        }
+        connection.query(
+          'SELECT ssn FROM USER WHERE id=?;',
+          [id],
+          (error1, data1) => {
+            if (error1) throw error;
+            res.send(true);
+          },
+          connection.query(
+            'INSERT INTO INJECTION(number, ssn, orgcd, inject_date, reservation_time) VALUES (?,?,?,NULL,?)',
+            [result[0].number, data1.ssn, orgcd, reservation_time],
+            (error2, data2) => {
+              if (error2) throw error;
+              res.send(true);
+            },
+          ),
+        );
+        console.log(result);
+      },
+    );
+  });
+};
