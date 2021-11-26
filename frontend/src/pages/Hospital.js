b65cbf74ac3a60cd23f5cebe5a3cd217d09c662f
Woony <clover7kso@naver.com>
2021-11-26 19:16:40 +0900

    fixing Hospital

--- frontend/src/pages/Hospital.js
+++ frontend/src/pages/Hospital.js
@@ -19,11 +19,7 @@ import call from '../imgs/call.svg';
 import loc from '../imgs/location.svg';
 import time from '../imgs/time.svg';
 
-const Body = styled.div`
-  display: flex;
-  flex-direction: horizontal;
-`;
-const Col = styled.div``;
+const Body = styled.div``;
 
 const ItemBody = styled.div`
   padding: 32px 32px 0px;
@@ -147,8 +143,10 @@ const Hospital = ({ history }) => {
           setOrginfo(data);
         });
       axios
-        .get('http://localhost:4000/hospitalReserve', { params: { orgcd : hcode, date : new Date() }})
-        .then(( tmdata ) => {
+        .get('http://localhost:4000/hospitalReserve', {
+          params: { orgcd: hcode, date: new Date() },
+        })
+        .then((tmdata) => {
           setNine(tmdata.data['09']);
           setTen(tmdata.data['10']);
           setEleven(tmdata.data['11']);
@@ -164,172 +162,161 @@ const Hospital = ({ history }) => {
 
   return (
     <Body>
-      <Col style={{ paddingLeft: '200px', width: '20%' }}>
-        <CardWrapper
-          style={{
-            width: '100%',
-          }}
+      <CardWrapper
+        style={{
+          width: '100%',
+        }}
+      >
+        <CardBody>
+          <CardFieldset style={{ paddingTop: 32 }}>
+            <CardTitle>접종기관 검색</CardTitle>
+          </CardFieldset>
+          <CardFieldset>
+            <CardTitle>병원명</CardTitle>
+            <CardInput
+              placeholder="병원명을 입력해주세요"
+              type="text"
+              onChange={(e) => setOrgnm(e.target.value)}
+              value={orgnm}
+            />
+          </CardFieldset>
+          <CardFieldset>
+            <CardTitle>시/도</CardTitle>
+            <CardSelect
+              defaultValue="강원도"
+              onChange={(e) => setSelSido(e.target.value)}
+            >
+              {sido.map((item) => {
+                return (
+                  <CardSelectOption value={item.sido}>
+                    {item.sido}
+                  </CardSelectOption>
+                );
+              })}
+            </CardSelect>
+          </CardFieldset>
+          <CardFieldset>
+            <CardTitle>시/구</CardTitle>
+            <CardSelect onChange={(e) => setSelSi(e.target.value)}>
+              {si.map((item) => {
+                return (
+                  <CardSelectOption value={item.si}>{item.si}</CardSelectOption>
+                );
+              })}
+            </CardSelect>
+          </CardFieldset>
+        </CardBody>
+      </CardWrapper>
+
+      <CardWrapper
+        style={{
+          padding: '32px 0 0px',
+          width: '100%',
+        }}
+      >
+        <CardBody>
+          <CardFieldset>
+            <CardTitle>
+              {selSido} {selSi}의 접종기관 목록
+            </CardTitle>
+          </CardFieldset>
+        </CardBody>
+
+        <InfiniteScroll
+          height="350px"
+          dataLength={hostpitals.length} //This is important field to render the next data
+          next={next}
+          hasMore={true}
+          loader={
+            <ItemBody>
+              <ItemTitle style={{ paddingBottom: '20px' }}>
+                불러오는 중...
+              </ItemTitle>
+            </ItemBody>
+          }
         >
-          <CardHeader style={{ marginBottom: '0px' }}>
-            <CardHeading>접종기관 검색</CardHeading>
-          </CardHeader>
-
-          <CardBody>
-            <CardFieldset>
-              <CardTitle>병원명</CardTitle>
-              <CardInput
-                placeholder="병원명을 입력해주세요"
-                type="text"
-                onChange={(e) => setOrgnm(e.target.value)}
-                value={orgnm}
-              />
-            </CardFieldset>
-
-            <CardFieldset>
-              <CardTitle>시/도</CardTitle>
-              <CardSelect
-                defaultValue="강원도"
-                onChange={(e) => setSelSido(e.target.value)}
-              >
-                {sido.map((item) => {
-                  return (
-                    <CardSelectOption value={item.sido}>
-                      {item.sido}
-                    </CardSelectOption>
-                  );
-                })}
-              </CardSelect>
-            </CardFieldset>
-
-            <CardFieldset>
-              <CardTitle>시/구</CardTitle>
-              <CardSelect onChange={(e) => setSelSi(e.target.value)}>
-                {si.map((item) => {
-                  return (
-                    <CardSelectOption value={item.si}>
-                      {item.si}
-                    </CardSelectOption>
-                  );
-                })}
-              </CardSelect>
-            </CardFieldset>
-          </CardBody>
-        </CardWrapper>
-
-        <CardWrapper
-          style={{
-            padding: '32px 0 0px',
-            width: '100%',
-          }}
-        >
-          <CardBody>
-            <CardFieldset>
-              <CardTitle>
-                {selSido} {selSi}의 접종기관 목록
-              </CardTitle>
-            </CardFieldset>
-          </CardBody>
-
-          <InfiniteScroll
-            height="350px"
-            dataLength={hostpitals.length} //This is important field to render the next data
-            next={next}
-            hasMore={true}
-            loader={
+          {hostpitals.map((item) => {
+            return (
               <ItemBody>
-                <ItemTitle style={{ paddingBottom: '20px' }}>
-                  불러오는 중...
-                </ItemTitle>
+                <ItemButton
+                  onClick={(e) => {
+                    setHcode(item.orgcd);
+                  }}
+                >
+                  <ItemMain>
+                    <ItemAddr>{item.sido + ' > ' + item.si}</ItemAddr>
+                    <ItemTitle>{item.orgnm}</ItemTitle>
+                  </ItemMain>
+                </ItemButton>
+                <ItemIcon width="16px" position="right" />
               </ItemBody>
-            }
-          >
-            {hostpitals.map((item) => {
-              return (
-                <ItemBody>
-                  <ItemButton
-                    onClick={(e) => {
-                      setHcode(item.orgcd);
-                    }}
-                  >
-                    <ItemMain>
-                      <ItemAddr>{item.sido + ' > ' + item.si}</ItemAddr>
-                      <ItemTitle>{item.orgnm}</ItemTitle>
-                    </ItemMain>
-                  </ItemButton>
-                  <ItemIcon width="16px" position="right" />
-                </ItemBody>
-              );
-            })}
-          </InfiniteScroll>
-        </CardWrapper>
-      </Col>
+            );
+          })}
+        </InfiniteScroll>
+      </CardWrapper>
 
-      <Col style={{ paddingLeft: '40px', paddingRight: '200px', width: '80%' }}>
-        <CardWrapper
-          style={{
-            height: '91%',
-            width: '100%',
-          }}
-        >
-          <CardHeader style={{ marginBottom: '0px' }}>
-            <CardHeading>접종기관 조회</CardHeading>
-          </CardHeader>
-          <CardBody>
-            {orginfo.map((data) => {
-              return (
-                <CardFieldset>
-                  <h2 align="left"> {data.orgnm} </h2>
-                  <table width="100%" border="0">
-                    <tr>
-                      <td colSpan="2" align="left">
-                        <p>
-                          화이자 {data.canSelectVaccine.화이자} | 모더나{' '}
-                          {data.canSelectVaccine.모더나} | 얀센{' '}
-                          {data.canSelectVaccine.얀센} | 아스트라제네카{' '}
-                          {data.canSelectVaccine.아스트라제네카}
-                        </p>
-                      </td>
-                    </tr>
-                    <tr width="32px" height="32px">
-                      <td style={{ width: '32px' }}>
-                        <img src={loc} width="32px" alt="" />
-                      </td>
-                      <td align="left">{data.orgZipaddr}</td>
-                    </tr>
-                    <tr width="32px" height="32px">
-                      <td style={{ width: '32px' }}>
-                        <img src={call} width="32px" alt="" />
-                      </td>
-                      <td align="left">{data.orgTlno}</td>
-                    </tr>
-                    <tr width="32px" height="32px">
-                      <td style={{ width: '32px' }}>
-                        <img src={time} width="32px" alt="" />
-                      </td>
-                      <td align="left">
-                        {data.sttTm} ~ {data.endTm}
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                
-                        </td>
-                      09 : {nine};
-                    </tr>
-                    <tr>
-                      <td colSpan="2" align="center">
-                        <CardButton style = {{ width: '40%' }}>
-                          잔여백신 당일예약하기
-                        </CardButton>
-                      </td>
-                    </tr>
-                  </table>
-                </CardFieldset>
-              );
-            })}
-          </CardBody>
-        </CardWrapper>
-      </Col>
+      <CardWrapper
+        style={{
+          marginBottom: '32px',
+          width: '100%',
+        }}
+      >
+        <CardBody>
+          <CardFieldset style={{ paddingTop: 32 }}>
+            <CardTitle>접종기관 검색</CardTitle>
+          </CardFieldset>
+          {orginfo.map((data) => {
+            return (
+              <CardFieldset>
+                <h2 align="left"> {data.orgnm} </h2>
+                <table width="100%" border="0">
+                  <tr>
+                    <td colSpan="2" align="left">
+                      <p>
+                        화이자 {data.canSelectVaccine.화이자} | 모더나{' '}
+                        {data.canSelectVaccine.모더나} | 얀센{' '}
+                        {data.canSelectVaccine.얀센} | 아스트라제네카{' '}
+                        {data.canSelectVaccine.아스트라제네카}
+                      </p>
+                    </td>
+                  </tr>
+                  <tr width="32px" height="32px">
+                    <td style={{ width: '32px' }}>
+                      <img src={loc} width="32px" alt="" />
+                    </td>
+                    <td align="left">{data.orgZipaddr}</td>
+                  </tr>
+                  <tr width="32px" height="32px">
+                    <td style={{ width: '32px' }}>
+                      <img src={call} width="32px" alt="" />
+                    </td>
+                    <td align="left">{data.orgTlno}</td>
+                  </tr>
+                  <tr width="32px" height="32px">
+                    <td style={{ width: '32px' }}>
+                      <img src={time} width="32px" alt="" />
+                    </td>
+                    <td align="left">
+                      {data.sttTm} ~ {data.endTm}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td></td>
+                    09 : {nine};
+                  </tr>
+                  <tr>
+                    <td colSpan="2" align="center">
+                      <CardButton style={{ width: '40%' }}>
+                        잔여백신 당일예약하기
+                      </CardButton>
+                    </td>
+                  </tr>
+                </table>
+              </CardFieldset>
+            );
+          })}
+        </CardBody>
+      </CardWrapper>
     </Body>
   );
 };
