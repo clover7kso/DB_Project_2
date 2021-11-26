49bdb390ad4467cff09e3302ab0f5b4b2ac5bd27
JEONGWOO-C <dnsgmlehdtod@gmail.com>
2021-11-27 01:17:14 +0900

    hospital design required

--- frontend/src/pages/Hospital.js
+++ frontend/src/pages/Hospital.js
@@ -15,6 +15,7 @@ import axios from 'axios';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import styled from 'styled-components';
 import { ReactComponent as arrow_right } from '../imgs/arrow_left.svg';
+import org from '../imgs/hospital.svg'
 import call from '../imgs/call.svg';
 import loc from '../imgs/location.svg';
 import time from '../imgs/time.svg';
@@ -62,6 +63,23 @@ const ItemButton = styled.button`
   }
   &:active {
     background: #c8c8c8;
+  }
+`;
+
+const TimeButton = styled.button`
+  width : '20%';
+  border : 2px solid #CDCDCD;
+  background: ${(props) => props.background || "gray"};
+  cursor : pointer;
+  display: block;
+  left : 0%;
+  right : auto;
+  position: relative;
+  transition: all 0.2s cubic-bezier(0.42, 0, 0.58, 1);
+
+  &:hover {
+    background-color: #c8c8c8;
+    text-shadow: nthree;
   }
 `;
 
@@ -125,7 +143,7 @@ const Hospital = ({ history }) => {
       });
   };
 
-  var [orginfo, setOrginfo] = useState([]);
+  var [orginfo, setOrginfo] = useState();
   var [nine, setNine] = useState(0);
   var [ten, setTen] = useState(0);
   var [eleven, setEleven] = useState(0);
@@ -140,25 +158,37 @@ const Hospital = ({ history }) => {
       axios
         .get('http://localhost:4000/hospitalInfo', { params: { orgcd: hcode } })
         .then(({ data }) => {
-          setOrginfo(data);
+          setOrginfo(data[0]);
         });
       axios
         .get('http://localhost:4000/hospitalReserve', {
-          params: { orgcd: hcode, date: new Date() },
-        })
+          params: { orgcd: hcode, date: new Date() } })
         .then((tmdata) => {
           setNine(tmdata.data['09']);
           setTen(tmdata.data['10']);
           setEleven(tmdata.data['11']);
           setTwelve(tmdata.data['12']);
-          setOne(tmdata.data['01']);
-          setTwo(tmdata.data['02']);
-          setThree(tmdata.data['03']);
-          setFour(tmdata.data['04']);
-          setFive(tmdata.data['05']);
+          setOne(tmdata.data['13']);
+          setTwo(tmdata.data['14']);
+          setThree(tmdata.data['15']);
+          setFour(tmdata.data['16']);
+          setFive(tmdata.data['17']);
         });
     }
   }, [hcode]);
+
+  var [revTm, setRevTm] = useState(0);
+  var [revVaccine, setRevVaccine] = useState('');
+
+  const divRef=useRef(false);
+  function handleChecked(target) {
+    
+    const vCheck = divRef.current.getElementsByTagName("input");
+    
+    for(var i=0; i<vCheck.length; i++)
+      vCheck[i].checked = false;
+    target.checked = true;
+  }
 
   return (
     <Body>
@@ -197,7 +227,7 @@ const Hospital = ({ history }) => {
           </CardFieldset>
           <CardFieldset>
             <CardTitle>시/구</CardTitle>
-            <CardSelect onChange={(e) => setSelSi(e.target.value)}>
+            <CardSelect onChange={(e) => setSelSi(e.target)}>
               {si.map((item) => {
                 return (
                   <CardSelectOption value={item.si}>{item.si}</CardSelectOption>
@@ -262,59 +292,96 @@ const Hospital = ({ history }) => {
         }}
       >
         <CardBody>
-          <CardFieldset style={{ paddingTop: 32 }}>
-            <CardTitle>접종기관 검색</CardTitle>
+          <CardFieldset>
+            <CardTitle>선택한 접종기관 정보</CardTitle>
           </CardFieldset>
-          {orginfo.map((data) => {
-            return (
+          <CardFieldset>
+            {orginfo!=null?(
               <CardFieldset>
-                <h2 align="left"> {data.orgnm} </h2>
-                <table width="100%" border="0">
-                  <tr>
-                    <td colSpan="2" align="left">
-                      <p>
-                        화이자 {data.canSelectVaccine.화이자} | 모더나{' '}
-                        {data.canSelectVaccine.모더나} | 얀센{' '}
-                        {data.canSelectVaccine.얀센} | 아스트라제네카{' '}
-                        {data.canSelectVaccine.아스트라제네카}
-                      </p>
-                    </td>
-                  </tr>
-                  <tr width="32px" height="32px">
-                    <td style={{ width: '32px' }}>
-                      <img src={loc} width="32px" alt="" />
-                    </td>
-                    <td align="left">{data.orgZipaddr}</td>
-                  </tr>
-                  <tr width="32px" height="32px">
-                    <td style={{ width: '32px' }}>
-                      <img src={call} width="32px" alt="" />
-                    </td>
-                    <td align="left">{data.orgTlno}</td>
-                  </tr>
-                  <tr width="32px" height="32px">
-                    <td style={{ width: '32px' }}>
-                      <img src={time} width="32px" alt="" />
-                    </td>
-                    <td align="left">
-                      {data.sttTm} ~ {data.endTm}
-                    </td>
-                  </tr>
-                  <tr>
-                    <td></td>
-                    09 : {nine};
-                  </tr>
-                  <tr>
-                    <td colSpan="2" align="center">
-                      <CardButton style={{ width: '40%' }}>
-                        잔여백신 당일예약하기
-                      </CardButton>
-                    </td>
-                  </tr>
-                </table>
-              </CardFieldset>
-            );
-          })}
+                <CardTitle style = {{ fontSize : '24px' }}>
+                  <img src = {org} width = '32px' height = '32px'/>
+                  {orginfo.orgnm}
+                </CardTitle>
+                <CardTitle>
+                  <img src = {loc} width = '32px' height = '32px'/>
+                  {orginfo.orgZipaddr}
+                </CardTitle>
+                <CardTitle>
+                  <img src = {call} width = '32px' height = '32px'/>
+                  {orginfo.orgTlno}
+                </CardTitle>
+                <CardTitle>
+                  <img src = {time} width = '32px' height = '32px'/>
+                  {orginfo.sttTm.substr(3,7)} ~ {orginfo.endTm.substr(3,7)}
+                </CardTitle>
+                <CardTitle>{orginfo.lunchSttTm.substr(0,2)}:{orginfo.lunchSttTm.substr(2,4)} ~ {orginfo.lunchEndTm.substr(0,2)}:{orginfo.lunchEndTm.substr(2,4)}</CardTitle>
+                <CardTitle style = {{ color : 'gray' }}>
+                  화이자 : {orginfo.canSelectVaccine.화이자} |
+                  모더나 : {orginfo.canSelectVaccine.모더나} |
+                  얀센 : {orginfo.canSelectVaccine.얀센} |
+                  아스트라제네카 : {orginfo.canSelectVaccine.아스트라제네카}
+                </CardTitle>
+                <div className = 'setRevVaccine' ref = {divRef}>
+                  <input type= 'checkbox' id = 'pfizer' 
+                  onClick={(e)=>handleChecked(e.target)}/>
+                  <label for='pfizer'>화이자</label>
+                  <input type= 'checkbox' id = 'moderna' 
+                  onClick={(e)=>handleChecked(e.target)}/>
+                  <label for='moderna'>모더나</label>
+                  <input type= 'checkbox' id = 'jansen' 
+                  onClick={(e)=>handleChecked(e.target)}/>
+                  <label for='jansen'>얀센</label>
+                  <input type= 'checkbox' id = 'az' 
+                  onClick={(e)=>handleChecked(e.target)}/>
+                  <label for='az'>아스트라제네카</label>
+                </div>
+                <div className = 'setRevTime'>
+                  <CardButton style = {{ backgroundColor : nine > 0? 'green':'gray' }}
+                  onclick = {(e) => { setRevTm(9) }}>
+                  9시 ~ 10시 남은 예약수 : {nine}
+                  </CardButton>
+                  <CardButton style = {{ backgroundColor : ten > 0? 'green':'gray' }}
+                  onclick = {(e) => { setRevTm(10)}}>
+                  10시 ~ 11시 남은 예약수 : {ten}
+                  </CardButton>
+                  <CardButton style = {{ backgroundColor : eleven > 0? 'green':'gray' }}
+                  onclick = {(e) => {setRevTm(11) }}>
+                  11시 ~ 12시 남은 예약수 : {eleven}
+                  </CardButton>
+                  <CardButton style = {{ backgroundColor : twelve > 0? 'green':'gray' }}
+                  onclick = {(e) => { setRevTm(12) }}>
+                  12시 ~ 13시 남은 예약수 : {twelve}
+                  </CardButton>
+                  <CardButton style = {{ backgroundColor : one > 0? 'green':'gray' }}
+                  onclick = {(e) => { setRevTm(13) }}>
+                  13시 ~ 14시 남은 예약수 : {one}
+                  </CardButton>
+                  <CardButton style = {{ backgroundColor : two > 0? 'green':'gray' }}
+                  onclick = {(e) => { setRevTm(14) }}>
+                  14시 ~ 15시 남은 예약수 : {two}
+                  </CardButton>
+                  <CardButton style = {{ backgroundColor : three > 0? 'green':'gray' }}
+                  onclick = {(e) => { setRevTm(15) }}>
+                  15시 ~ 16시 남은 예약수 : {three}
+                  </CardButton>
+                  <CardButton style = {{ backgroundColor : four > 0? 'green':'gray' }}
+                  onclick = {(e) => { setRevTm(16) }}>
+                  16시 ~ 17시 남은 예약수 : {four}
+                  </CardButton>
+                  <CardButton style = {{ backgroundColor : five > 0? 'green':'gray' }}
+                  onclick = {(e) => { setRevTm(17) }}>
+                  17시 ~ 18시 남은 예약수 : {five}
+                  </CardButton>
+                </div>
+                <div className = 'revBtn' align = 'center'>
+                  <CardButton style={{ width: '40%'}}>
+                    잔여백신 당일예약하기
+                  </CardButton>
+                </div>
+              </CardFieldset>)
+              :null
+            }
+          </CardFieldset>
         </CardBody>
       </CardWrapper>
     </Body>
