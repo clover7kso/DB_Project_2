68fb6ea4a6bdb597c8ae3e9ec49749d86836b9e9
rang21c <rang21c@naver.com>
2021-11-27 02:51:30 +0900

    reserveVaccine

--- backend/src/api/Vaccine/reserveVaccine.js
+++ backend/src/api/Vaccine/reserveVaccine.js
@@ -1,6 +1,10 @@
+import { sign } from '../modules/jwt.js';
+import auth from "../../modules/auth.js";
+
 export default (app, connection) => {
-    app.post('/reserveVaccine', (req, res, next) => {
-      const { vaccine_name, ssn, orgcd, reservation_time } = req.body;
+    app.get('/reserveVaccine', auth);
+    app.use('/reserveVaccine', (req, res, next) => {
+      const { vaccine_name, orgcd, reservation_time, id } = req.query;
        connection.query(
         'SELECT number, name FROM VACCINE V WHERE V.name = ? AND NOT EXISTS(SELECT number FROM INJECTION I WHERE I.number = V.number)',
         [vaccine_name],
@@ -12,12 +16,20 @@ export default (app, connection) => {
                 res.send(false);
             }
             connection.query(
-                'INSERT INTO INJECTION(number, ssn, orgcd, inject_date, reservation_time) VALUES (?,?,?,NULL,?)',
-                [result[0].number, ssn, orgcd, reservation_time],
+                'SELECT ssn FROM USER WHERE id=?;',
+                [id],
                 (error1, data1) => {
                     if (error1) throw error;
                     res.send(true);
                 },
+                connection.query(
+                    'INSERT INTO INJECTION(number, ssn, orgcd, inject_date, reservation_time) VALUES (?,?,?,NULL,?)',
+                    [result[0].number, data1.ssn, orgcd, reservation_time],
+                    (error2, data2) => {
+                        if (error2) throw error;
+                        res.send(true);
+                    },
+                )
             );
             console.log(result);
         },
