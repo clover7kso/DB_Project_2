d093701a0065baf9b01b3ab9708f587495ffe9ff
rang21c <rang21c@naver.com>
2021-11-27 11:19:01 +0900

    Fix hospital Reserve

--- backend/src/api/Hospital/hospitalReserve.js
+++ backend/src/api/Hospital/hospitalReserve.js
@@ -1,76 +1,53 @@
 export default async (app, connection) => {
+  var result = [
+    {'key':9, 'value':0},
+    {'key':10, 'value':0},
+    {'key':11, 'value':0},
+    {'key':12, 'value':0},
+    {'key':13, 'value':0},
+    {'key':14, 'value':0},
+    {'key':15, 'value':0},
+    {'key':16, 'value':0},
+    {'key':17, 'value':0},
+  ];
+
   app.get('/hospitalReserve', async (req, res, next) => {
-    const { orgcd, date } = req.query;
-    await connection.query(
-      'SELECT I.orgcd, DATE_FORMAT(I.reservation_time, "%Y.%m.%d") AS reserve_date, HOUR(I.reservation_time) AS reserve_hour, H.maxCapacityperhour, H.lunchSttTm/100 AS lunchStt, H.lunchEndTm/100 AS lunchEnt FROM INJECTION I NATURAL JOIN HOSPITAL H WHERE I.orgcd=?;',
+    const { orgcd, y,m,d } = req.query;
+    connection.query(
+      'select lunchSttTm ls, lunchEndTm le, maxCapacityperhour max from hospital where orgcd=?;',
       [orgcd],
-      (error, data) => {
-        if (error) console.log(error);
-        const result = data;
-        //console.log(result);
-        if (result.length === 0) {
-          var Reserve = {
-            orgcd: orgcd,
-            date: date,
-            '09': 10,
-            10: 10,
-            11: 10,
-            12: 10,
-            13: 10,
-            14: 10,
-            15: 10,
-            16: 10,
-            17: 10,
-          };
-          connection.query(
-            'SELECT lunchSttTm/100 AS lunchStt1, lunchEndTm/100 AS lunchEnt1 FROM HOSPITAL WHERE orgcd=?;',
-            [orgcd],
-            (error1, data1) => {
-              if (error1) console.log(error1);
-              const result1 = data1;
-              var lunch_start = result1[0].lunchStt1;
-              for (
-                var i = 0;
-                i < result1[0].lunchEnt1 - result1[0].lunchStt1;
-                i++
-              ) {
-                //점심시간 처리
-                Reserve[lunch_start++] = 0;
-              }
-              //console.log(Reserve);
-              return res.send(Reserve);
-            },
-          );
-        } else {
-          var maxCap = result[0].maxCapacityperhour;
-          var Reserve = {
-            orgcd: orgcd,
-            date: date,
-            '09': maxCap,
-            10: maxCap,
-            11: maxCap,
-            12: maxCap,
-            13: maxCap,
-            14: maxCap,
-            15: maxCap,
-            16: maxCap,
-            17: maxCap,
-          };
-          for (var i = 0; i < result.length; i++) {
-            if (result[i].reserve_date === Reserve.date) {
-              //일치하는 날짜일경우 예약되어 있는 시간개수만큼 여석 줄여줌
-              Reserve[result[i].reserve_hour]--;
-            }
-          }
-          var lunch_start = result[0].lunchStt;
-          for (var i = 0; i < result[0].lunchEnt - result[0].lunchStt; i++) {
-            //점심시간 처리
-            Reserve[lunch_start++] = 0;
-          }
-          //console.log(Reserve);
-          return res.send(Reserve);
+      (error1, data1) => {
+        if (error1) console.log(error1);
+
+        //점심시간 삭제
+        var lunch_start = data1[0].ls-9;
+        var lunch_end = data1[0].le-9;
+        var deleteNum = lunch_end-lunch_start;
+        result.splice(lunch_start, deleteNum);
+
+        //Max로 세팅
+        const max = data1[0].max;
+        for(var i = 0; i<result.length;i++)
+        {
+          result[i].value=max;
         }
-      },
-    );
-  });
-};
+
+        connection.query(
+          'with oymdh_max as (select i.orgcd o, year(reservation_time) y ,month(reservation_time) m,day(reservation_time) d,hour(reservation_time) h,count(*) c, h.maxCapacityperhour max from injection i left join hospital h on i.orgcd=h.orgcd group by o,y,m,d,h) select o,y,m,d,h,c,max,max-c r from oymdh_max where y=? and m=? and d =? and o=?;',
+          [y,m,d,orgcd],
+          (error2, data2) => {
+            if (error2) console.log(error2);
+
+            for(var i=0; i<result.length; i++){
+              for(var j=0; j<data2.length; j++){
+                if(result[i].key === data2[j].h){
+                  result[i].value = data2[j].r;
+                }
+              }
+            }
+
+            res.send(result);
+        })
+    }) 
+  })
+}
