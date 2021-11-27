6ddb39c56559316dd504b85adac41a126ad008fa
izar111 <ghktks98@gmail.com>
2021-11-27 09:17:41 +0900

    MyPage Component Design Change #3

--- frontend/src/components/MyPageComp.js
+++ frontend/src/components/MyPageComp.js
@@ -2,50 +2,65 @@ import React from 'react';
 import {
   CardWrapper,
   CardBody,
+  CardTitle,
   CardHeader,
   CardHeading,
   CardFieldset,
   CardLink,
 } from '../components/Card';
 import styled from 'styled-components';
+import org from '../imgs/hospital.svg';
+import loc from '../imgs/location.svg';
+import call from '../imgs/call.svg';
+import time from '../imgs/time.svg';
+import lunch from '../imgs/lunch.svg';
+import unsel_inject from '../imgs/unsel_inject.png';
 
 const Col = styled.div`
   align-items: 'center';
   justify-content: 'center';
 `;
 
-const Table = styled.table`
-  border:1;
-  border-color:'white';
-  display:'flex';
-  width:80%;
-  margin-left:35px;
-  margin-bottom:20px;
+
+const Body = styled.div`
+  margin-top: 12px;
 `;
 
-const T_Col = styled.tr`
-  width:100%;
+const ItemWrapper = styled.div`
+  padding-bottom: 4px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
 `;
 
-const T_Row = styled.td`
-  background-color:#FAFAFA;
-  padding-top:5px;
-  padding-bottom:5px;
-  width:100%;
+const ItemName = styled.div`
+  padding-top: 0.1em;
+  padding-left: 1em;
+  font-family: inherit;
+  text-align: left;
+  width: 20%;
+  font-size: 12px;
+  color: #2c2c2c;
+`;
+const ItemInfo = styled.div`
+  padding-top: 0.1em;
+  padding-left: 1em;
+  font-family: inherit;
+  text-align: left;
+  width: 100%;
+  font-size: 12px;
+  color: #2c2c2c;
+`;
+const ItemIcon = styled.img`
+  width: 16px;
+  height: 16px;
 `;
 
-const T_RowBold = styled.th`
-  background-color:#EEEEEE;
-  padding-top:5px;
-  padding-bottom:5px;
-  width:30%;
-`;
 
-const T_RowTitle = styled.th`
-  background-color:#E2E2E2;
-  padding-top:5px;
-  padding-bottom:5px;
-  width:70%;
+const ItemEmptyIcon = styled.rect`
+  width: 16px;
+  height: 16px;
 `;
 
 const DateTransForm = (raw_date)=>{
@@ -58,11 +73,13 @@ class DataLine extends React.Component{
     render(){
         var name = this.props.title;
         var value = this.props.value;
+        var icon = this.props.icon;
         return(
-            <T_Col>
-                <T_RowBold>{name}</T_RowBold>
-                <T_Row>{value}</T_Row>
-            </T_Col>
+          <ItemWrapper>
+            {icon===undefined?'':<ItemIcon src={icon} />}
+            <ItemName>{name}</ItemName>
+            <ItemInfo> : {value}</ItemInfo>
+          </ItemWrapper>
         );
     }
 }
@@ -75,7 +92,7 @@ class InjectResult extends React.Component{
         const inj = value.inject_date;
         const rsv = value.reservation_time;
         const vname = value.Vname;
-        const org = value.orgnm;
+        const orgnm = value.orgnm;
         //const number = value.number;
 
         var i_str = DateTransForm(inj);
@@ -84,16 +101,16 @@ class InjectResult extends React.Component{
         //var inject_date = inj.split('T').split('');
 
         return(
-            <Table>
-                <T_Col>
-                    <T_RowTitle colSpan="2">{count}차 접종 {inj!==null?'완료':'예정'}</T_RowTitle>
-                </T_Col>
-                <DataLine title="접종 기관" value={org}/>
-                <DataLine title="백신명" value={vname}/>
+          <CardBody>
+            <CardFieldset>
+              <CardTitle style={count>1?{ marginTop: '2em' }:{}}>{count}차 접종 정보{inj===null?'(예정)':'(완료)'}</CardTitle>
+                <DataLine title="접종 기관" value={orgnm} icon={org}/>
+                <DataLine title="백신명" value={vname} icon={unsel_inject}/>
                 {/*<DataLine title="백신 일련번호" value={number}/>*/}
-                <DataLine title="백신 예약일자" value={r_str}/>
-                {inj===null?'':<DataLine title="백신 접종일자" value={i_str}/>}
-            </Table>
+                <DataLine title="백신 예약일자" value={r_str} icon={time}/>
+                {inj===null?'':<DataLine title="백신 접종일자" value={i_str} icon={time}/>}
+              </CardFieldset>
+            </CardBody>
         );
     }
 }
@@ -104,7 +121,7 @@ class InjectList extends React.Component{
         return(
             <CardWrapper style={{width:'100%'}}>
               <CardHeader>
-                <CardHeading>접종 기록</CardHeading>
+                <CardHeading style={{marginBottom:'0px'}}>접종 기록</CardHeading>
               </CardHeader>
               <CardBody style={{width:'100%'}}>
                 
@@ -123,29 +140,30 @@ class PersonList extends React.Component{
         var Data = this.props.value;
         return(
         <CardWrapper style={{width:'100%'}}>
-        <CardHeader>
-        <CardHeading>회원 정보</CardHeading>
-        </CardHeader>
+          <CardHeader>
+            <CardHeading style={{marginBottom:'0px'}}>회원 정보</CardHeading>
+          </CardHeader>
+          <CardBody style={{width:'100%'}}>
+          <CardBody>
+            <CardFieldset style={{marginLeft:'32px'}}>
+              <CardTitle>인적 사항</CardTitle>
+              <DataLine title='이름' value={Data.name}/>
+              <DataLine title='주민등록번호' value={Data.ssn}/>
+              <DataLine title='전화번호' value={Data.phone}/>
+              <DataLine title='지역' value={Data.sido}/>
+            </CardFieldset>
 
-        <CardBody style={{width:'100%'}}>
-        <Table>
-            <T_Col>
-                <T_RowTitle colSpan="2">인적사항</T_RowTitle>
-            </T_Col>
-            <DataLine title='이름' value={Data.name}/>
-            <DataLine title='주민등록번호' value={Data.ssn}/>
-            <DataLine title='전화번호' value={Data.phone}/>
-            <DataLine title='지역' value={Data.sido}/>
-            <T_Col>
-                <T_RowTitle colSpan="2">아이디/패스워드</T_RowTitle>
-            </T_Col>
-            <DataLine title='아이디' value={Data.id}/>
-            <DataLine title='비밀번호' value={"*".repeat(Data.pw.length)}/>
-        </Table>
-        <CardFieldset>
-            <CardLink link="/UserUpdate">정보 수정을 원하시나요?</CardLink>
-        </CardFieldset>
-        </CardBody>
+            <CardFieldset style={{marginLeft:'32px'}}>
+              <CardTitle style={{ marginTop: '2em' }}>아이디/비밀번호</CardTitle>
+              <DataLine title='아이디' value={Data.id}/>
+              <DataLine title='비밀번호' value={"*".repeat(Data.pw.length)}/>
+            </CardFieldset>
+          </CardBody>
+
+            <CardFieldset style={{ marginTop: '2em',marginLeft:'-64px'}}>
+              <CardLink link="/UserUpdate">정보 수정을 원하시나요?</CardLink>
+            </CardFieldset>
+          </CardBody>
         </CardWrapper>
         );
     }
@@ -160,10 +178,10 @@ export class MyPageComp extends React.Component {
 
     if(Data!==undefined && injData!==undefined){
     return (
-      <Col>
+      <div>
         <PersonList value={Data}/>
         <InjectList value={injData}/>
-      </Col>
+      </div>
     );
     }else return <div>로딩 중....</div>;
   }
